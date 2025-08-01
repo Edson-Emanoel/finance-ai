6ce12bc3291298f9575a35136757126269104305
Edson-Emanoel <edson.emanoelofc@gmail.com>
2025-08-01 18:28:15 -0300

    feat: Adicionei o componente de Últimas Transações(LastTransactions)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,6 +7,7 @@ import { isMatch } from "date-fns";
 import TransactionsPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "../_data/get-dashboard";
 import ExpensesPerCategory from "./_components/expenses-per-category";
+import LastTransactions from "./_components/last-transactions";
 
 interface HomeProps {
   searchParams: {
@@ -48,6 +49,8 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
               <ExpensesPerCategory expensesPerCategory={dashboard.totalExpensePerCategory} />
             </div>
           </div>
+          
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
